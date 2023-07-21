d90579648bc3bd679fb11eb4946fc4ae27b72f44
plathis1 <plathis1@binghamton.edu>
2023-07-20 20:56:30 -0700

    latest changes

--- main.tf
+++ main.tf
@@ -1,6 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
 provider "azurerm" {
-    version = "3.0.0"
     features {}
+}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "terraform_blob"
+        storage_account_name = "tformstoragestate"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
+
+variable "imagebuild" {
+  type        = string
+  description = "Latest Image Build"
 }
 
 resource "azurerm_resource_group" "tf_test" {
@@ -19,7 +41,7 @@ resource "azurerm_container_group" "tfcg_test" {
 
   container {
       name            = "weatherapi"
-      image           = "lathish97/my-dotnet-app"
+      image           = "lathish97/my-dotnet-app:${var.imagebuild}"
         cpu             = "1"
         memory          = "1"
 
